67a116a39dd3c9575a1d96b903ba24378f2a0c5d
vishangi <vishangid@gmail.com>
2022-07-25 15:45:58 +0530

    Added : Creating a new contact from JDBC

--- main/java/com/bridgelabz/AddressBookJDBC.java
+++ main/java/com/bridgelabz/AddressBookJDBC.java
@@ -16,9 +16,22 @@ public class AddressBookJDBC {
             connection = DriverManager.getConnection(jdbcURL, userName, passWord);
             System.out.println("Connection is successful!!!!" + connection);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select count(*) from address_book_service.address_book where state='Kerala'");
+            statement.execute("insert into address_book_service.address_book values('Vihan','Dabhade','Kon','Pen','Kerala','vih@gmail.com','625321','451245','2019-10-03','Man','Profession')");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM address_book_service.address_book");
+            System.out.println("\nRetrieve address Book contact from database :");
+            System.out.println();
             while (resultSet.next()) {
-                System.out.println("Kerala city count in address Book :" + resultSet.getInt(1));
+                System.out.println(resultSet.getString(1)
+                        + " " + resultSet.getString(2)
+                        + " " + resultSet.getString(3)
+                        + " " + resultSet.getString(4)
+                        + " " + resultSet.getString(5)
+                        + " " + resultSet.getString(6)
+                        + " " + resultSet.getInt(7)
+                        + " " + resultSet.getInt(8)
+                        + " " + resultSet.getDate(9)
+                        + " " + resultSet.getString(10)
+                        + " " + resultSet.getString(11));
             }
         } catch (Exception exception) {
             System.out.println(exception.getMessage());
